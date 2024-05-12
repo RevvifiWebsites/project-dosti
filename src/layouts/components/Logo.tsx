82e732a3983fd0a4a82ef03204a63fbde9f6ecf5
AyushB08 <abheemaiah194@student.fuhsd.org>
2024-05-11 21:51:18 -0700

    placeholder "DOSTI" logo

--- src/layouts/components/Logo.tsx
+++ src/layouts/components/Logo.tsx
@@ -35,19 +35,10 @@ const Logo = ({ src }: { src?: string }) => {
   const logoPath = src ? src : resolvedLogo;
 
   return (
+    
     <Link href="/" className="navbar-brand inline-block">
       {logoPath ? (
-        <Image
-          width={logo_width.replace("px", "") * 2}
-          height={logo_height.replace("px", "") * 2}
-          src={logoPath}
-          alt={title}
-          priority
-          style={{
-            height: logo_height.replace("px", "") + "px",
-            width: logo_width.replace("px", "") + "px",
-          }}
-        />
+        <span>DOSTI</span>
       ) : logo_text ? (
         logo_text
       ) : (
